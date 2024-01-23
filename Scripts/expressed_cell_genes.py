dfe13a80aea24fc1928df0fd5ed368e3d215c955
AnaValente <anavalenteebirdll@gmail.com>
2024-01-23 17:46:21 +0000

    Update expressed_cell_genes.py

--- Scripts/expressed_cell_genes.py
+++ Scripts/expressed_cell_genes.py
@@ -28,16 +28,21 @@ def cell_common_genes(df,tpm,tpm5):
 
 if __name__ == "__main__":
 
-    cell_tpm = pd.read_csv(sys.argv[3], skiprows=4, sep='\t')
-    
-    # convert the df gene names to HGNC nomenclature
-    gp = GProfiler(return_dataframe=True)
-    gp = gp.convert(organism='hsapiens',query=cell_tpm['Gene ID'].values.tolist(),target_namespace='HGNC')
-    cell_tpm = cell_tpm.join(gp['converted']).drop(['Gene Name'],axis=1)
-    cell_tpm5 = cell_tpm[cell_tpm.iloc[:, 1] >= 5.0] # select only the genes with more than 5 TPM
+    if sys.argv[3] != 'false':
+        cell_tpm = pd.read_csv(sys.argv[3], skiprows=4, sep='\t')
+        
+        # convert the df gene names to HGNC nomenclature
+        gp = GProfiler(return_dataframe=True)
+        gp = gp.convert(organism='hsapiens',query=cell_tpm['Gene ID'].values.tolist(),target_namespace='HGNC')
+        cell_tpm = cell_tpm.join(gp['converted']).drop(['Gene Name'],axis=1)
+        cell_tpm5 = cell_tpm[cell_tpm.iloc[:, 1] >= 5.0] # select only the genes with more than 5 TPM
 
-    ctrl_NM_genes = pd.read_csv(sys.argv[2]+'_'+sys.argv[1]+'_refseq.bedgraph', sep='\t',header=None,names=['chr','start','end','mean_methylation_dif','Gene Name','dist'])
-    common_genes_NM = cell_common_genes(ctrl_NM_genes,cell_tpm,cell_tpm5)
+        ctrl_NM_genes = pd.read_csv(sys.argv[2]+'_'+sys.argv[1]+'_refseq.bedgraph', sep='\t',header=None,names=['chr','start','end','mean_methylation_dif','Gene Name','dist'])
+        common_genes_NM = cell_common_genes(ctrl_NM_genes,cell_tpm,cell_tpm5)
 
-    common_genes_NM.to_csv(sys.argv[2]+'_'+sys.argv[1]+'_merged_genes.bedgraph',index=False,header=False,sep='\t')
-    common_genes_NM['Gene Name'].to_csv(sys.argv[2]+'_'+sys.argv[1]+'_merged_genes.txt',index=False,header=False,sep='\t')
+        common_genes_NM.to_csv(sys.argv[2]+'_'+sys.argv[1]+'_merged_genes.bedgraph',index=False,header=False,sep='\t')
+        common_genes_NM['Gene Name'].to_csv(sys.argv[2]+'_'+sys.argv[1]+'_merged_genes.txt',index=False,header=False,sep='\t')
+    else:
+        ctrl_NM_genes = pd.read_csv(sys.argv[2]+'_'+sys.argv[1]+'_refseq.bedgraph', sep='\t',header=None,names=['chr','start','end','mean_methylation_dif','Gene Name','dist'])
+        ctrl_NM_genes.to_csv(sys.argv[2]+'_'+sys.argv[1]+'_merged_genes.bedgraph',index=False,header=False,sep='\t')
+        ctrl_NM_genes['Gene Name'].to_csv(sys.argv[2]+'_'+sys.argv[1]+'_merged_genes.txt',index=False,header=False,sep='\t')
