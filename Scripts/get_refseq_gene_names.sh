9b45099ef384a32e961b18f234070472889d12c3
AnaValente <anavalenteebirdll@gmail.com>
2024-01-23 17:47:17 +0000

    Update get_refseq_gene_names.sh

--- Scripts/get_refseq_gene_names.sh
+++ Scripts/get_refseq_gene_names.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash 
 
-# download RefSeq genes
-curl -s "https://hgdownload.cse.ucsc.edu/goldenpath/hg38/database/ncbiRefSeq.txt.gz" | gunzip -c | cut -f3,5,6,13 | sort-bed - > genes.bed
-
-# search for overlaping coordinates between RefSeq and DMRs and store in bedgraph with gene names
-sort-bed $1* | bedtools closest -D ref -a - -b genes.bed | cut -f1,2,3,4,8,9 | awk '!x[$0]++' - > $2_refseq.bedgraph
+# search for closest coordinates between RefSeq and DMRs and store in bedgraph with gene names
+sort-bed $1 | bedtools closest -D ref -a - -b RefSeq_genes.bed | cut -f1,2,3,4,8,9 | awk '!x[$0]++' - > $2_refseq.bedgraph
 
 
