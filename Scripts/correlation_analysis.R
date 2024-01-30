534014a0792c05cfb771a5371b0d9adb89d44d4c
AnaValente <anavalenteebirdll@gmail.com>
2024-01-30 16:28:59 +0000

    Update correlation_analysis.R

--- Scripts/correlation_analysis.R
+++ Scripts/correlation_analysis.R
@@ -9,7 +9,7 @@ library("scales")
 
 args = commandArgs(trailingOnly = TRUE)
 
-correlation = function(df, n_samples, n_replicates,control) {
+correlation = function(df, n_samples, n_replicates, control) {
 
   df = read.csv(df, sep = "\t")
   df = df[, 4:ncol(df)] # remove string collumns
@@ -23,19 +23,28 @@ correlation = function(df, n_samples, n_replicates,control) {
   summ = summary(pca)
 
   scores = as.data.frame(pca$rotation)
-  scores = scores[1:3] # choose only the first principal components
+  
+  if (n_replicates <= 1 & n_samples <= 2) {
+    scores = scores[1:2] # choose only the first two principal components
+  }else{
+    scores = scores[1:3] # choose only the first three principal components
+  }
 
   colors = c()
 
-  for (i in hue_pal()(as.integer(n_samples)+1)) {
-    colors =  c(colors,rep(i,n_replicates))
-  }
-
-  
-  if(paste(control,'_rep2',sep = '') %in% colnames(df)) {
-    colors = colors
+  if (n_replicates > 1){
+    for (i in hue_pal()(as.integer(n_samples)+1)) {
+      colors =  c(colors,rep(i,n_replicates))
+    }
   }else{
+    for (i in hue_pal()(as.integer(n_samples)+1)) {
+      colors =  c(colors,i)
+  }}
+  
+  if (!(paste(control,'_rep2',sep = '') %in% colnames(df)) & any(grepl("rep2", colnames(df)))) {
     colors = colors[-1]
+  }else{
+    colors = colors
   }
 
   ggplot(scores, aes(PC1, PC2)) + geom_point(colour = colors, size = 2) +
@@ -44,12 +53,13 @@ correlation = function(df, n_samples, n_replicates,control) {
     theme_linedraw() + theme(axis.line = element_line(color = "black"), plot.background = element_blank(), panel.grid.minor = element_blank(), panel.grid.major = element_blank()) # plot of the PCA scores of PC1 and PC2
   ggsave("PCA_analysis_PC1_PC2.png", width = 5, height = 4, dpi = 400)
 
-  ggplot(scores, aes(PC2, PC3)) + geom_point(colour = colors, size = 2) +
-    geom_text_repel(aes(label = rownames(scores)), size = 3) + 
-    labs(x = sprintf("PC2 (%s%% of the variance explained)", round(summ$importance[2, 2], digits = 2) * 100), y=sprintf("PC3 (%s%% of the variance explained)", round(summ$importance[2, 3], digits = 2) * 100)) + 
-    theme_linedraw() + theme(axis.line = element_line(color = "black"), plot.background = element_blank(), panel.grid.minor = element_blank(), panel.grid.major = element_blank())# plot of the PCA scores of PC2 and PC3
-  ggsave("PCA_analysis_PC2_PC3.png", width = 5, height = 4, dpi = 400)
-
+  if (n_replicates > 1 & n_samples > 2){
+    ggplot(scores, aes(PC2, PC3)) + geom_point(colour = colors, size = 2) +
+      geom_text_repel(aes(label = rownames(scores)), size = 3) + 
+      labs(x = sprintf("PC2 (%s%% of the variance explained)", round(summ$importance[2, 2], digits = 2) * 100), y=sprintf("PC3 (%s%% of the variance explained)", round(summ$importance[2, 3], digits = 2) * 100)) + 
+      theme_linedraw() + theme(axis.line = element_line(color = "black"), plot.background = element_blank(), panel.grid.minor = element_blank(), panel.grid.major = element_blank())# plot of the PCA scores of PC2 and PC3
+    ggsave("PCA_analysis_PC2_PC3.png", width = 5, height = 4, dpi = 400)
+    }
   fviz_eig(pca, addlabels = TRUE, ylim = c(0, 100)) + theme_bw() + theme(plot.background = element_blank(), panel.grid.minor = element_blank(), panel.grid.major = element_blank()) + scale_y_continuous(expand = c(0, 0)) # scree plot
   ggsave("PCA_scree_plot.png", width = 6, height = 5, dpi = 400)
 
