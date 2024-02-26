6f771be1e1996bc6b1716e7e14ad29d5db486346
AnaValente <51082691+AnaValente@users.noreply.github.com>
2024-02-26 11:43:58 +0000

    Delete Scripts/merge_bams_samtools.sh

--- Scripts/merge_bams_samtools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# merge bams of different runs
-samtools merge $1_library.bam $1*.bam && samtools sort $1_library.bam -o results/$1_library_sorted.bam -O BAM &&  samtools index results/$1_library_sorted.bam
-
-rm $1_library.bam 
