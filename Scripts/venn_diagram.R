942bdf63dbd76cf65c894579bdd76b6b56541e4c
AnaValente <anavalenteebirdll@gmail.com>
2024-03-05 15:07:58 +0000

    Update venn_diagram.R

--- Scripts/venn_diagram.R
+++ Scripts/venn_diagram.R
@@ -21,10 +21,9 @@ diagram = function() {
   sample_names = lapply(sample_names, function(i) {sub("./", "", as.character(i))})
 
   # plot veen diagram
-  diagram = ggVennDiagram(genes_matrix, label = "count", label_alpha = 0, label_size = 6, set_size = 5, category.names = sample_names, edge_size = 0) + scale_fill_gradient(low = "#F4FAFE", high = "#3f648e")
+  diagram = ggVennDiagram(genes_matrix, label = "count", label_alpha = 0, label_size = 6, set_size = 6, category.names = sample_names, edge_size = 0) + scale_fill_gradient(low = "#F4FAFE", high = "#3f648e") + scale_x_continuous(expand = expansion(mult = .2))
 
-  ggsave("venn_diagram.png", diagram, scale = 2, width = 15, height = 10, dpi = 200, units = c("cm"), bg = "White")
+  ggsave("venn_diagram.png", diagram, scale = 2, width = 15, height = 10, dpi = 400, units = c("cm"), bg = "White")
 }
-
 
 diagram()
