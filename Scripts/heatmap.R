17dff402b47ded56dc60c33bb34d88af40c0030a
AnaValente <anavalenteebirdll@gmail.com>
2024-04-23 14:44:03 +0100

    Update heatmap.R

--- Scripts/heatmap.R
+++ Scripts/heatmap.R
@@ -6,22 +6,22 @@ args = commandArgs(trailingOnly = TRUE)
 
 heatmap = function(df, filename) {
 
-  # merge CpG coordinates
+  # Merge CpG coordinates
   df[, "CpG_sites"] = paste(df$chr, ":", df$start, "-", df$end, sep = "")
 
-  # change DataFrame names to CpG coordinates
+  # Change DataFrame names to CpG coordinates
   row.names(df) = df[, "CpG_sites"]
   df = df[, 4:(ncol(df)-1)]
   
-  # plot histogram
+  # Plot histogram
   reso = 400
   length = 3.25*reso/72
   png(filename, units="in", res=reso, height=length, width=length)
-  heatmap.2(as.matrix(df), srtCol = 360, adjCol = c(0.5, 0), cexRow = 0.8, margins = c(3, 12.5), hclustfun = hclust, trace = "none", col = colorpanel(100, "red3", "gray", "blue4"))
+  heatmap.2(as.matrix(df), srtCol = 360, adjCol = c(0.5, 0), cexRow = 0.8, margins = c(3, 12.5), hclustfun = hclust,
+  trace = "none", col = colorpanel(100, "red3", "gray", "blue4"))
   dev.off()
 
 }
 
 df_methylation = read.csv(args[1], sep = "\t")
-
 heatmap(df_methylation, "Methylation_heatmap.png")
