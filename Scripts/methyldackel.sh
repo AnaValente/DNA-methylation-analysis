15ac62a234cc8a26f3ed22183e7c49db3b67a88a
AnaValente <anavalenteebirdll@gmail.com>
2024-06-24 13:51:05 +0100

    Update methyldackel.sh

--- Scripts/methyldackel.sh
+++ Scripts/methyldackel.sh
@@ -4,12 +4,16 @@ for i in $(seq 1 $1)
 do
     if compgen -G "${2}_rep${i}_*bam" > /dev/null; then
         if compgen -G "${2}_rep${i}_library_sorted.bam" > /dev/null; then
-            MethylDackel extract --mergeContext -l CpGIslands_mod.bed hg38.fa ${2}_rep${i}_library_sorted.bam
-            MethylDackel extract --mergeContext -l CpGIslands_mod.bed --fraction hg38.fa ${2}_rep${i}_library_sorted.bam
+            methyl="$(MethylDackel mbias -l CpGIslands_mod.bed hg38.fa ${2}_rep${i}_library_sorted.bam ${2}_rep${i} 2>&1)"
+            condition="$(echo ${methyl} | sed -e 's/^.*://g')"
+            MethylDackel extract --mergeContext -l CpGIslands_mod.bed hg38.fa ${condition} ${2}_rep${i}_library_sorted.bam
+            #MethylDackel extract --mergeContext -l CpGIslands_mod.bed --fraction hg38.fa ${2}_rep${i}_library_sorted.bam
         else
             mv ${2}_rep${i}*.bam ${2}_rep${i}_library_sorted.bam
-            MethylDackel extract --mergeContext -l CpGIslands_mod.bed hg38.fa ${2}_rep${i}_library_sorted.bam
-            MethylDackel extract --mergeContext -l CpGIslands_mod.bed --fraction hg38.fa ${2}_rep${i}_library_sorted.bam
+            methyl="$(MethylDackel mbias -l CpGIslands_mod.bed hg38.fa ${2}_rep${i}_library_sorted.bam ${2}_rep${i} 2>&1)"
+            condition="$(echo ${methyl} | sed -e 's/^.*://g')"
+            MethylDackel extract --mergeContext -l CpGIslands_mod.bed hg38.fa ${condition} ${2}_rep${i}_library_sorted.bam
+            #MethylDackel extract --mergeContext -l CpGIslands_mod.bed --fraction hg38.fa ${2}_rep${i}_library_sorted.bam
         fi
     fi
     
