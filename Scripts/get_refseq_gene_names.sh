808d8ab372864e0c81d3a9d7fe5a189ebf9b606b
AnaValente <51082691+AnaValente@users.noreply.github.com>
2024-09-26 14:00:36 +0100

    Update to include different genomes

--- Scripts/get_refseq_gene_names.sh
+++ Scripts/get_refseq_gene_names.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash 
 
 # search for closest coordinates between RefSeq and DMRs and store in bedgraph with gene names
-sort-bed $1 | bedtools closest -D ref -a - -b RefSeq_genes.bed | cut -f1,2,3,4,8,9 | awk '!x[$0]++' - > $2_refseq.bedgraph
-
-
+sort-bed $1 | bedtools closest -D ref -a - -b  $2 | cut -f1,2,3,4,8,9 | awk '!x[$0]++' - > $3_refseq.bedgraph
