cf437aa2c554177bb0536745681c9358ffac911e
AnaValente <51082691+AnaValente@users.noreply.github.com>
2024-10-08 16:19:15 +0100

    Update methyldackel.sh

--- Scripts/methyldackel.sh
+++ Scripts/methyldackel.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-for i in $(seq 1 $1)
-do
-    if compgen -G "${2}_rep${i}*bam" > /dev/null; then
-        if compgen -G "${2}_rep${i}_library_sorted.bam" > /dev/null; then
-            methyl="$(MethylDackel mbias -l CpGIslands_mod.bed genome.fa ${2}_rep${i}_library_sorted.bam ${2}_rep${i} 2>&1)"
-            condition="$(echo ${methyl} | sed -e 's/^.*://g')"
-            MethylDackel extract --mergeContext -l CpGIslands_mod.bed genome.fa ${condition} ${2}_rep${i}_library_sorted.bam
-        else
-            mv ${2}_rep${i}*.bam ${2}_rep${i}_library_sorted.bam
-            methyl="$(MethylDackel mbias -l CpGIslands_mod.bed genome.fa ${2}_rep${i}_library_sorted.bam ${2}_rep${i} 2>&1)"
-            condition="$(echo ${methyl} | sed -e 's/^.*://g')"
-            MethylDackel extract --mergeContext -l CpGIslands_mod.bed genome.fa ${condition} ${2}_rep${i}_library_sorted.bam
-        fi
+check_bam_file() {
+    compgen -G "$1" > /dev/null
+}
+
+process_bam_file() {
+    local bam_file=$1
+    local prefix=$2
+    local library_sorted_bam="${prefix}_library_sorted.bam"
+    local methyl
+    local condition
+
+    # Check if library_sorted.bam exists, if not, rename existing bam file
+    if ! check_bam_file "${library_sorted_bam}"; then
+        mv ${prefix}*.bam ${library_sorted_bam}
     fi
-    
+
+    # Run MethylDackel mbias and extract condition
+    if [ "$3" == "false" ]; then
+        methyl="$(MethylDackel mbias genome.fa ${library_sorted_bam} ${prefix} 2>&1)"
+    else
+        methyl="$(MethylDackel mbias -l $3 genome.fa ${library_sorted_bam} ${prefix} 2>&1)"
+    fi
+
+    condition="$(echo ${methyl} | sed -e 's/^.*://g')"
+
+    # Run MethylDackel extract
+    if [ "$3" == "false" ]; then
+        MethylDackel extract --mergeContext genome.fa ${condition} ${library_sorted_bam}
+    else
+        MethylDackel extract --mergeContext -l $3 genome.fa ${condition} ${library_sorted_bam}
+    fi
+}
+
+for i in $(seq 1 $1); do
+    prefix="${2}_rep${i}"
+
+    if check_bam_file "${prefix}*bam"; then
+        process_bam_file "${prefix}" "${prefix}" "$3"
+    fi
 done
